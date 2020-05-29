bdf701292301ccb3f7fce8f98d1d2f271321b47d
tetuaoro <tetuaoropro@gmail.com>
2020-05-28 15:44:25 -1000

    add: repo ; upd: fix css, remove border-bottom and fix error when chord isn't exist

--- src/App.js
+++ src/App.js
@@ -1,14 +1,18 @@
-import React, { Component } from 'react';
+import React, { Component } from "react";
 
-import Lyrics from './components/Lyrics/Lyrics';
+import Lyrics from "./components/Lyrics/Lyrics";
 
-import './App.css';
+import "./App.css";
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Lyrics isEmbedChord={true}>[F] Good morning!</Lyrics>
+        <Lyrics isEmbedChord={true}>Je me sens libre[G]</Lyrics>
+        <Lyrics isEmbedChord={true}>Je suis fragil[Fm]e</Lyrics>
+        <Lyrics isEmbedChord={true}>[Cm] C[E]omment m'as-[Am]tu abondoné</Lyrics>
+        <Lyrics isEmbedChord={true}>Mais ou est donc or ni car</Lyrics>
+        <Lyrics isEmbedChord={true}>[C]Hey man ou est donc or[N] ni car</Lyrics>
       </div>
     );
   }
--- src/components/Lyrics/Lyrics.jsx
+++ src/components/Lyrics/Lyrics.jsx
@@ -1,14 +1,14 @@
-import React, { PureComponent } from 'react';
-import PropTypes from 'prop-types';
+import React, { PureComponent } from "react";
+import PropTypes from "prop-types";
 
 // Import custom components
-import Chord from '../Chord/Chord';
+import Chord from "../Chord/Chord";
 
 // Import styles from css
-import './styles.css';
+import "./styles.css";
 
 // Import chords dataSource
-import chords from '../../assets/chords';
+import chords from "../../assets/chords";
 
 // Define regular expression to get the chord
 const chordRegExp = /(?:\[([^\]]*)])?(.)?([^[]*)/g;
@@ -17,7 +17,7 @@ const englishRegExp = /[a-zA-Z]/;
 const searchChord = (name) => {
   let chordsLen = chords.length;
 
-  for (let i = 0 ; i < chordsLen ; i++) {
+  for (let i = 0; i < chordsLen; i++) {
     if (chords[i].name === name) {
       return chords[i];
     }
@@ -25,7 +25,6 @@ const searchChord = (name) => {
 };
 
 export default class Lyrics extends PureComponent {
-
   /**
    * Method to map chords inside the string
    */
@@ -35,16 +34,15 @@ export default class Lyrics extends PureComponent {
 
     // Extract information from a piece of lyrics
     line.replace(chordRegExp, (match, chord, anchor, extra, index) => {
-
       // Filter empty line
       if (chord || (anchor && extra)) {
         chords.push({
           name: chord,
           line: line,
           index: chord ? chord.length + index + offset : -1,
-          anchor: anchor ? anchor : '',
-          extra: extra ? extra : ''
-        })
+          anchor: anchor ? anchor : "",
+          extra: extra ? extra : "",
+        });
       }
     });
 
@@ -57,66 +55,78 @@ export default class Lyrics extends PureComponent {
   mapAnchorStyle = (anchor, index) => {
     return {
       width: englishRegExp.test(anchor) ? 10 : 15,
-    }
+    };
   };
 
   render() {
-    const {
-      children,
-      isEmbedChord,
-    } = this.props;
+    const { children, isEmbedChord } = this.props;
+
+    const style = {
+      backgroundColor: "red",
+      color: "white",
+    };
 
     return (
       <div>
         {/* The offset to split each lyrics when the chord is embeded */}
-        {
-          isEmbedChord && <div style={{height: 30}}/>
-        }
+        {isEmbedChord && <div style={{ height: 30 }} />}
         <p className="ge-lyrics">
           {
             // Map each chord data to the lyrics
             this.mapChords(children).map((chordData, index) => {
               let chord = searchChord(chordData.name);
 
-              return (
-              chordData.index > 0 ?
-                // Start with chord
-                <span className="ge-chord-item" key={index}>
-                  <span
-                    style={this.mapAnchorStyle(chordData.anchor, chordData.index)}
-                    className="ge-anchor">
-                    {/* Anchor chord */}
-                    {
-                      isEmbedChord ?
-                      <span className="ge-anchor-chordimg">
-                        <Chord chord={chord} options={{size: 'small'}}/>
-                      </span>
-                      :
-                      <span className="ge-anchor-chordname">{chordData.name}</span>
-                    }
-                    {/* Anchor Text */}
-                    <span>{chordData.anchor}</span>
+              return chordData.index > 0 ? (
+                // if chord exist
+                chord ? (
+                  // Start with chord
+                  <span className="ge-chord-item" key={index}>
+                    <span
+                      style={this.mapAnchorStyle(
+                        chordData.anchor,
+                        chordData.index
+                      )}
+                      className="ge-anchor"
+                    >
+                      {/* Anchor chord */}
+                      {isEmbedChord ? (
+                        <span className="ge-anchor-chordimg">
+                          <Chord chord={chord} options={{ size: "small" }} />
+                        </span>
+                      ) : (
+                        <sup className="ge-anchor-chordname">
+                          {chordData.name}
+                        </sup>
+                      )}
+                      {/* Anchor Text */}
+                      <span className="gd-anchor">{chordData.anchor}</span>
+                    </span>
+                    <span className="ge-extra">{chordData.extra}</span>
                   </span>
-                  <span className="ge-extra">{chordData.extra}</span>
-                </span>
-                :
+                ) : (
+                  <span key={index} style={style}>
+                    Verifier vos notes de musiques.
+                  </span>
+                )
+              ) : (
                 // Normal sentence
                 <span key={index}>
                   <span>{chordData.anchor}</span>
                   <span>{chordData.extra}</span>
                 </span>
-            )})
+              );
+            })
           }
         </p>
       </div>
-    )
+    );
   }
 }
 
 Lyrics.propTypes = {
-  isEmbedChord: PropTypes.bool
+  isEmbedChord: PropTypes.bool,
 };
 
 Lyrics.defaultProps = {
-  isEmbedChord: false
+  isEmbedChord: false,
 };
