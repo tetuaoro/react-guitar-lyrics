a652e32375cbe8b78df704d83122298411926816
tetuaoro <tetuaoropro@gmail.com>
2020-05-30 21:18:57 -1000

    Upd: <pre> element use instead <p>. Chords are in abolute position to the relative parent <pre>. <pre> element overflow-x when the size is too small.

--- src/App.js
+++ src/App.js
@@ -8,18 +8,15 @@ class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      embed: false,
       config: {
         color: "",
         fontWeight: 0,
       },
     };
-    this.handleEmbedChord = this.handleEmbedChord.bind(this);
   }
 
   componentDidMount() {
     this.setState({
-      embed: false,
       config: {
         color: "green",
         fontWeight: 520,
@@ -27,32 +24,21 @@ class App extends Component {
     });
   }
 
-  handleEmbedChord() {
-    this.setState({ embed: !this.state.embed });
-  }
-
   render() {
-    const { embed, config } = this.state;
+    const { config } = this.state;
     return (
       <div className="App">
-        <label htmlFor="checkbox">Embed chord ?</label>
-        <input
-          type="checkbox"
-          id="checkbox"
-          defaultChecked={false}
-          onChange={this.handleEmbedChord}
-        />
-        <Lyrics isEmbedChord={embed} chordStyle={config}>
-          Je me[G] sens libre
-        </Lyrics>
-        <Lyrics isEmbedChord={embed} chordStyle={config}>
-          [C]Et je suis, t[F]u es, et [Gm]rien
-        </Lyrics>
-        <Lyrics isEmbedChord={embed} chordStyle={config}>Je m[F]i sens libre[F] </Lyrics>
-        <Lyrics isEmbedChord={embed} chordStyle={config}>Je me[J] sens libre</Lyrics>
-        <Lyrics isEmbedChord={embed} chordStyle={config}>Je me sens libr[G]e</Lyrics>
-        <Lyrics isEmbedChord={embed} chordStyle={config}>Je me l[F]e sens [G]libre</Lyrics>
-        <Lyrics isEmbedChord={embed} chordStyle={config}>[G]   [D] Je suis donc [G] </Lyrics>
+        <div className="item-lyrics">
+          <Lyrics chordStyle={config}>Je me[G] sens libre</Lyrics>
+          <Lyrics chordStyle={config}>
+            [C]Et je suis, t[F]u es, et [Gm]rien
+          </Lyrics>
+          <Lyrics chordStyle={config}>Je m[F]i sens libre[F]</Lyrics>
+          <Lyrics chordStyle={config}>[C]Je me[J] sens libre</Lyrics>
+          <Lyrics chordStyle={config}>Je me sens libr[G]e</Lyrics>
+          <Lyrics chordStyle={config}>Je me l[F]e sens [G]libre</Lyrics>
+          <Lyrics chordStyle={config}>[G]   [D]Je suis donc [G]</Lyrics>
+        </div>
       </div>
     );
   }
--- src/components/Lyrics/Lyrics.jsx
+++ src/components/Lyrics/Lyrics.jsx
@@ -1,8 +1,5 @@
 import React, { PureComponent } from "react";
-import PropTypes from "prop-types";
-
-// Import custom components
-import Chord from "../Chord/Chord";
+import PropTypes from 'prop-types';
 
 // Import styles from css
 import "./styles.css";
@@ -12,7 +9,6 @@ import chords from "../../assets/chords";
 
 // Define regular expression to get the chord
 const chordRegExp = /(?:\[([^\]]*)])?(.)?([^[]*)/g;
-const isMajorChar = /[A-Z]/;
 
 const searchChord = (name) => {
   let chordsLen = chords.length;
@@ -28,10 +24,7 @@ const searchChord = (name) => {
  *
  * Rendering your chords between bracket and styling with custom props
  * 
- * @param {Boolean} [isEmbedChord=false] - 2 display modes : letter chord or image svg chord
- * @param {Object} [chordStyle] - custom chord with color and font-weight
- * @param {String} [chordStyle.color] - the color 
- * @param {Number} [chordStyle.fontWeight] - the font-weight 
+ * @param {Object} [chordStyle] - styling with your own css
  *
  * @export
  * @class Lyrics
@@ -67,94 +60,55 @@ export default class Lyrics extends PureComponent {
     return chords;
   };
 
-  /**
-   * Give a style to anchor according to its type
-   */
-  mapAnchorStyle = (anchor, index) => {
-    return {
-      width: isMajorChar.test(anchor) ? 0.8 + 'em' : 0.4 + 'em',
-    };
-  };
-
   render() {
-    const { children, isEmbedChord, chordStyle } = this.props;
 
-    const style = {
-      display: "inline-block",
-      backgroundColor: "red",
-      color: "white",
-    };
+    const { children, chordStyle } = this.props;
 
     return (
       <div>
-        {/* The offset to split each lyrics when the chord is embeded, uncomment to make it */}
-        {/* {isEmbedChord && <div style={{ height: 30 }} />} */}
-        <p className="ge-lyrics">
+        <pre className="ge-lyrics">
           {
-            // Map each chord data to the lyrics
             this.mapChords(children).map((chordData, index) => {
               let chord = searchChord(chordData.name);
-
               return chordData.index > 0 ? (
-                // if chord exist
                 chord ? (
-                  // Start with chord
                   <span className="ge-chord-item" key={index}>
-                    <span
-                      style={this.mapAnchorStyle(
-                        chordData.anchor,
-                        chordData.index
-                      )}
-                      className="ge-anchor"
-                    >
+                    <span className="ge-anchor">
                       {/* Anchor chord */}
-                      {isEmbedChord ? (
-                        <span className="ge-anchor-chordimg">
-                          <Chord chord={chord} options={{ size: "small" }} />
-                          <span className="ge-anchor-chordimg-large">
-                            <Chord chord={chord} options={{ size: "large" }} />
-                          </span>
-                        </span>
-                      ) : (
-                          <span className="ge-anchor-chordname" style={chordStyle}>
-                            {chordData.name}
-                          </span>
-                        )}
+                      <span className="ge-anchor-chordname" style={chordStyle}>
+                        {chordData.name}
+                      </span>
                       {/* Anchor Text */}
                       <span className="gd-anchor">{chordData.anchor}</span>
                     </span>
                     <span className="ge-extra">{chordData.extra}</span>
                   </span>
                 ) : (
-                    <span key={index} style={style}>
+                    // Error chord doesn't exist
+                    <span key={index} className="ge-error-chord">
                       Verifier vos notes de musiques.
                     </span>
                   )
               ) : (
                   // Normal sentence
-                  <span key={index}>
+                  <span key={index} className="ge-normal-text">
                     <span>{chordData.anchor}</span>
                     <span>{chordData.extra}</span>
                   </span>
                 );
             })
           }
-        </p>
+        </pre>
       </div>
     );
   }
-}
+};
 
 Lyrics.propTypes = {
-  isEmbedChord: PropTypes.bool,
-  chordStyle: PropTypes.shape({
-    color: PropTypes.string,
-    fontWeight: PropTypes.number,
-  })
+  chordStyle: PropTypes.object,
 };
 
 Lyrics.defaultProps = {
-  isEmbedChord: false,
   chordStyle: {
     color: 'indianred',
     fontWeight: 510,
